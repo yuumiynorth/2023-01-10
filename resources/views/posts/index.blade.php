8027ed244287b441700d2c2f65601b0157c09d0d
yuumiynorth <yuuminorth@gmail.com>
2023-01-10 22:33:33 +0900

    カリキュラム解答17. Laravel9基礎：ブログ投稿詳細の表示

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,15 +3,19 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        
     </head>
-    <body>
+    <body class="antialiased">
         <h1>Blog Name</h1>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+                <h2 class='title'>
+                    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                </h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
