0d6005e3961b9b7294121883c7c44cd52d270cf7
yuumiynorth <yuuminorth@gmail.com>
2023-01-22 12:38:31 +0900

    カリキュラム解答18. Laravel9基礎：ブログ投稿の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,8 +13,12 @@ class PostController extends Controller
     } 
     
     public function show(Post $post)
-{
-    return view('posts/show')->with(['post' => $post]);
- //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
-}
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('posts/create');
+    }
 }
